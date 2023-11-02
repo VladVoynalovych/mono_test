1ff0ba38cab3aa7d0b7cd86e20203ed0a1bfd9c1
VladVoynalovych <vlad.voynalovich@gmail.com>
2023-11-03 01:36:10 +0200

    update( RateProductForm ): props and emits

--- src/components/RateProductForm/index.vue
+++ src/components/RateProductForm/index.vue
@@ -6,51 +6,56 @@ import {
 } from "@/components/RateProduct/_utils/validationRules";
 import { FormType } from "@/components/RateProduct/_utils/types";
 
-const { name, email, phone, rate, review } = withDefaults(
-  defineProps<FormType>(),
-  {
-    name: "",
-    email: "",
-    phone: "+380",
-    rate: "",
-    review: "",
-  },
-);
+withDefaults(defineProps<FormType>(), {
+  name: "",
+  email: "",
+  phone: "+380",
+  rate: "",
+  review: "",
+});
 
-const emit = defineEmits();
+const emit = defineEmits<{
+  (e: "update:name", value: string): void;
+  (e: "update:email", value: string): void;
+  (e: "update:phone", value: string): void;
+  (e: "update:rate", value: string): void;
+  (e: "update:review", value: string): void;
+}>();
 </script>
 
 <template>
   <v-text-field
-    :model-value="name"
-    @on-update:model-value="(value) => emit('update:name', value)"
+    :model="name"
+    @update:model-value="(value: string) => emit('update:name', value)"
     :rules="nameRules"
     validate-on="blur"
     label="Name"
   ></v-text-field>
   <v-text-field
-    :model-value="email"
-    @on-update:model-value="(value) => emit('update:email', value)"
+    :model="email"
+    @update:model-value="(value: string) => emit('update:email', value)"
     label="Email"
     validate-on="blur"
     :rules="emailRules"
   ></v-text-field>
   <v-text-field
-    :model-value="phone"
-    @on-update:model-value="(value) => emit('update:phone', value)"
+    :model="phone"
+    @update:model-value="(value: string) => emit('update:phone', value)"
     label="Phone"
     validate-on="blur"
     :rules="phoneRules"
   ></v-text-field>
   <v-rating
-    :model-value="rate"
-    @on-update:model-value="(value) => emit('update:rate', value)"
+    :model="rate"
+    @update:model-value="
+      (value: number) => emit('update:rate', value.toString())
+    "
     size="small"
   ></v-rating>
   <v-textarea
     label="Product Review"
-    :model-value="review"
-    @on-update:model-value="(value) => emit('update:review', value)"
+    :model="review"
+    @update:model-value="(value: string) => emit('update:review', value)"
   ></v-textarea>
 </template>
 
