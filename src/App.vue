effead2da93d53ba71d7095a1cbea4bbea5cec1f
VladVoynalovych <vlad.voynalovich@gmail.com>
2023-11-02 02:09:13 +0200

    update( App.vue ): use router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <v-app>
     <v-main>
-      <RateProduct />
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
-<script setup lang="ts">
-import RateProduct from "@/components/RateProduct/index.vue";
-</script>
+<script setup lang="ts"></script>
