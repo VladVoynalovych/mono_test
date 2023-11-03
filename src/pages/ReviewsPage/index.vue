147498b4cf3290ba71a9d71416beaeba27e76e05
VladVoynalovych <vlad.voynalovich@gmail.com>
2023-11-03 02:52:39 +0200

    update( ReviewsPage ): redirect to the Create Review on button click

--- src/pages/ReviewsPage/index.vue
+++ src/pages/ReviewsPage/index.vue
@@ -4,18 +4,23 @@ import ReviewSection from "@/components/ReviewSection/index.vue";
 import { ref } from "vue";
 import db from "@/firebase/init";
 import { query, collection, getDocs } from "firebase/firestore";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const reviews = ref<FormType[]>([]);
 
 const getReviews = async () => {
   const reviewsDoc = await getDocs(query(collection(db, "reviews")));
   reviewsDoc.forEach((doc) => reviews.value.push(doc.data()));
 };
 
-const reviews = ref<FormType[]>([]);
 getReviews();
 </script>
 
 <template>
   <review-section :reviews="reviews" />
+  <v-btn @click="router.push('/')">Navigate to Create Review</v-btn>
 </template>
 
 <style scoped></style>
